df3db1d73f1554b33b3829f87abd29184f89e6c4
rootsUnicourse <rootsunicourse@gmail.com>
2024-11-14 15:30:32 +0200

    build(deps): update package-lock.json, package.json and App.js

---
+++ src/components/HomePage/HomePage.js
@@ -0,0 +1,13 @@
+// src/Home.js
+import React from 'react';
+
+function Home() {
+    return (
+        <div>
+            <h1>Welcome to the Home Page</h1>
+            <p>This is the main page of your application.</p>
+        </div>
+    );
+}
+
+export default Home;

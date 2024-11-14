4b7e0817eb40e7dbc4bb2d9fb7e1b7fbef96928d
rootsUnicourse <rootsunicourse@gmail.com>
2024-11-14 23:40:14 +0200

    test windows

--- src/components/HomePage/HomePage.js
+++ src/components/HomePage/HomePage.js
@@ -4,7 +4,7 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1>Welcome to the Home Page</h1>
+            <h1> to the Home Page</h1>
             <p>This is the main page of your application.</p>
         </div>
     );
